85aabb1b820226ab0b28bcec6a1f24778cb97adc
Alexandr <apuk1p@mail.ru>
2023-06-13 22:44:20 +0300

    added few methods

--- petProject.cpp
+++ petProject.cpp
@@ -1,5 +1,6 @@
 #include "vector3.h"
 #include <stdio.h>
+#include <vector>
 
 int check(int first, int second)
 {
@@ -13,38 +14,76 @@ int check(int first, int second)
 int main()
 {
     std::cout << "Hello World!\n";
-    Vector3<int> vec(3, 5, 2);
+    Vector3<float> vec(3, 5, 2);
+    Vector3<float> vec2(11, 11, 11);
+    Vector3<float> vec3(7, 7, 7);
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+    std::cout << std::endl;
+    //mul check
     vec.x *= 5;
     vec.z = vec.z * 3;
     vec.y *= 10;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+    std::cout << std::endl;
+
+    //sub check
     vec.x -= 3;
     vec.z = vec.z - 10;
     vec.y -= 35;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+    std::cout << std::endl;
 
+    //add check
     vec.x += 21;
     vec.z = vec.z + 2;
     vec.y += 11;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+    std::cout << std::endl;
 
+    //div check
     vec.x /= 3;
     vec.z = vec.z / 2;
     vec.y /= 2;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+    std::cout << std::endl;
 
-    Vector3<int> vec3(7, 7, 7);
+    //sub & add vectors check
     vec += vec3;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
-    Vector3<int> vec2(11, 11, 11);
     vec = vec - vec2;
     std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
-    /*Vector3<int> vec2(std::move(vec3));
-    std::cout << vec2.x << ' ' << vec2.y << ' ' << vec2.z << std::endl;
+    std::cout << std::endl;
+
+    //move check
+    Vector3<float> vec4(std::move(vec3));
     vec2 = std::move(vec);
-    std::cout << vec2.x << ' ' << vec2.y << ' ' << vec2.z << std::endl;
-    std::cout << vec3.x << ' ' << vec3.y << ' ' << vec3.z << std::endl;*/
+    std::cout << "Vec4 " << vec4.x << ' ' << vec4.y << ' ' << vec4.z << std::endl;
+    std::cout << "Vec2 " << vec2.x << ' ' << vec2.y << ' ' << vec2.z << std::endl;
+    std::cout << std::endl;
+
+    //normalize & magnitude check
+    float length = vec2.magnitude();
+    std::cout << "Vec2 magnitude " << length << std::endl;
+    vec2.normalize();
+    std::cout << "Vec2 normalize " << vec2.x << ' ' << vec2.y << ' ' << vec2.z << std::endl;
+    std::cout << std::endl;
+
+    //check cross product
+    Vector3<float> first(1, 0, 0);
+    Vector3<float> second(0, 1, 0);
+    Vector3<float> third;
+    third.crossProduct(first, second);
+
+    std::vector<Vector3<float>> pivot;
+    pivot.reserve(3);
+    pivot.push_back(first);
+    pivot.push_back(second);
+    pivot.push_back(third);
+
+    std::cout << "pivot X: " << pivot[0].x << ' ' << pivot[0].y << ' ' << pivot[0].z << std::endl;
+    std::cout << "pivot Y: " << pivot[1].x << ' ' << pivot[1].y << ' ' << pivot[1].z << std::endl;
+    std::cout << "pivot Z: " << pivot[2].x << ' ' << pivot[2].y << ' ' << pivot[2].z << std::endl;
+
     
     return 0;
 }
--- vector3.h
+++ vector3.h
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
+#include <cmath>
+#include <type_traits>
 
 template<typename Type>
-
 class Vector3
 {
 public:
@@ -15,83 +16,92 @@ public:
 	Vector3(Type x, Type y, Type z);
 	~Vector3();
 
-	Vector3(const Vector3& other);
-	Vector3(Vector3&& other) noexcept;
+	Vector3(const Vector3& vec);
+	Vector3(Vector3&& vec) noexcept;
 
-	Vector3& operator=(const Vector3& other);
-	Vector3& operator=(Vector3&& other) noexcept;
+	Vector3& operator=(const Vector3& vec);
+	Vector3& operator=(Vector3&& vec) noexcept;
 
-	Vector3& operator-=(const Vector3& other);
+	Vector3& operator-=(const Vector3& vec);
+	Vector3& operator+=(const Vector3& vec);
 
-	Vector3& operator+=(const Vector3& other);
-
-	Vector3& operator*(const Type value);
 	Vector3& operator*=(const Type value);
-
-	Vector3& operator/(const Type value);
 	Vector3& operator/=(const Type value);
 
-	void moveSwap(Vector3& first, Vector3& second);
+	void moveSwap(Vector3& vecF, Vector3& vecS);
+
+	float magnitude();
+	Vector3& normalize();
+	Vector3& crossProduct(const Vector3& vecF, const Vector3& vecS);
+	float dotProduct(const Vector3& vecF, const Vector3& vecS) const;
 
 };
 
 template<typename Type>
-void Vector3<Type>::moveSwap(Vector3<Type>& first, Vector3<Type>& second)
+float Vector3<Type>::dotProduct(const Vector3& vecF, const Vector3& vecS) const
 {
-	std::swap(first.x, second.x);
-	std::swap(first.y, second.y);
-	std::swap(first.z, second.z);
+
 }
 
 template<typename Type>
-Vector3<Type>::Vector3() : x(0), y(0), z(0) {}
+Vector3<Type>::Vector3() : x(0), y(0), z(0)
+{
+	static_assert(!std::is_integral_v<Type>, "Type shouldn't be integer");
+}
 
 template<typename Type>
 Vector3<Type>::Vector3(Type x, Type y, Type z)
 {
-	this->x = x;
-	this->y = y;
-	this->z = z;
+	if constexpr (std::is_floating_point_v<Type>)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	else
+	{
+		static_assert(!std::is_integral_v<Type>, "Type shouldn't be integer" );
+	}
 }
 
 template<typename Type>
 Vector3<Type>::~Vector3() {}
 
 template<typename Type>
-Vector3<Type>::Vector3(const Vector3<Type>& other)
+Vector3<Type>::Vector3(const Vector3<Type>& vec)
 {
-	x = other.x;
-	y = other.y;
-	z = other.z;
+	x = vec.x;
+	y = vec.y;
+	z = vec.z;
 }
 
 template<typename Type>
-Vector3<Type>::Vector3(Vector3<Type>&& other) noexcept : x(0),y(0),z(0)
+Vector3<Type>::Vector3(Vector3<Type>&& vec) noexcept : x(0),y(0),z(0)
 {
-	moveSwap(*this, other);
+	moveSwap(*this, vec);
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator=(const Vector3<Type>& other)
+Vector3<Type>& Vector3<Type>::operator=(const Vector3<Type>& vec)
 {
-	if (this != &other)
+	if (this != &vec)
 	{
-		x = other.x;
-		y = other.y;
-		z = other.z;
+		x = vec.x;
+		y = vec.y;
+		z = vec.z;
 	}
 	return *this;
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator=(Vector3<Type>&& other) noexcept
+Vector3<Type>& Vector3<Type>::operator=(Vector3<Type>&& vec) noexcept
 {
-	if (this != &other)
+	if (this != &vec)
 	{
 		this->x = 0;
 		this->y = 0;
 		this->z = 0;
-		moveSwap(*this, other);
+		moveSwap(*this, vec);
 	}
 	return *this;
 }
@@ -103,11 +113,11 @@ Vector3<Type> operator-(Vector3<Type> lhs, const Vector3<Type>& rhs)
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator-=(const Vector3<Type>& other)
+Vector3<Type>& Vector3<Type>::operator-=(const Vector3<Type>& vec)
 {
-	x -= other.x;
-	y -= other.y;
-	z -= other.z;
+	x -= vec.x;
+	y -= vec.y;
+	z -= vec.z;
 	return *this;
 }
 
@@ -118,31 +128,34 @@ Vector3<Type> operator+(Vector3<Type> lhs, const Vector3<Type>& rhs)
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator+=(const Vector3<Type>& other)
+Vector3<Type>& Vector3<Type>::operator+=(const Vector3<Type>& vec)
 {
-	x += other.x;
-	y += other.y;
-	z += other.z;
+	x += vec.x;
+	y += vec.y;
+	z += vec.z;
 	return *this;
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator*(const Type value)
+Vector3<Type> operator*(Vector3<Type> vec, const Type value)
 {
-	return (x * value, y * value, z * value);
+	return vec *= value;
 }
 
 template<typename Type>
 Vector3<Type>& Vector3<Type>::operator*=(const Type value)
 {
-	return (x *= value, y *= value, z *= value);
+	x *= value;
+	y *= value;
+	z *= value;
+	return *this;
 }
 
 template<typename Type>
-Vector3<Type>& Vector3<Type>::operator/(const Type value)
+Vector3<Type> operator/(Vector3<Type> vec, const Type value)
 {
 	_ASSERT(value != 0);
-	return (x / value, y / value, z / value);
+	return vec /= value;
 }
 
 template<typename Type>
@@ -153,5 +166,44 @@ Vector3<Type>& Vector3<Type>::operator/=(const Type value)
 		throw std::domain_error("Divide by zero exception!");
 	}*/
 	_ASSERT(value != 0);
-	return (x /= value, y /= value, z /= value);
+	x /= value;
+	y /= value;
+	z /= value;
+	return *this;
+}
+
+template<typename Type>
+void Vector3<Type>::moveSwap(Vector3<Type>& vecF, Vector3<Type>& vecS)
+{
+	std::swap(vecF.x, vecS.x);
+	std::swap(vecF.y, vecS.y);
+	std::swap(vecF.z, vecS.z);
+}
+
+template<typename Type>
+float Vector3<Type>::magnitude()
+{
+	float length = std::sqrtf((x * x) + (y * y) + (z * z)); 
+	return length;
+}
+
+template<typename Type>
+Vector3<Type>& Vector3<Type>::normalize()
+{
+	float length = this->magnitude();
+	x /= length;
+	y /= length;
+	z /= length;
+
+	return *this;
+}
+
+template<typename Type>
+Vector3<Type>& Vector3<Type>::crossProduct(const Vector3& vecF, const Vector3& vecS)
+{
+	x = vecF.y * vecS.z - vecF.z * vecS.y;
+	y = vecF.z * vecS.x - vecF.x * vecS.z;
+	z = vecF.x * vecS.y - vecF.y * vecS.x;
+
+	return *this;
 }
